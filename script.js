3550b4f02b3d78551cc3384be08e110b9afdc535
Logan <loganmedwards@lewisu.edu>
2023-03-01 00:37:31 -0600

    Moved submit button to bottom and added more js for Blood pressure + diseases

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-// Grab numbers from the text fields and pass them to the server
-// All calculations are done outside of script.js
+
 
 function grabNumbers() {
-    
+    // Grab numbers from the text fields and pass them to the server
+    // All calculations are done outside of script.js
     age = document.getElementById("Age").value;
     feet = document.getElementById("HeightFt").value;
     inches = document.getElementById("HeightIn").value;
     weight = document.getElementById("Weight").value;
-    console.log("age = "+ age + " height = " + feet + "ft " + inches + "in");
-    console.log("Weight = " + weight + "lbs");
-    
+    BPstage = document.getElementById("bplst").value;
+
+    // Each family disease is worth the same value of 10 points
+    // This will return the number of disease as 0-3
+    numDiseases = 0;
+    if (document.getElementById("Diabetes").checked == true){ numDiseases++; }
+    if (document.getElementById("Cancer").checked == true){ numDiseases++; }
+    if (document.getElementById("Alzheimers").checked == true){ numDiseases++; }
+
+    // Console log statements for testing feel free to uncomment 
+    // console.log("age: "+ age + " height: " + feet + "ft " + inches + "in");
+    // console.log("Weight : " + weight + "lbs");
+    // console.log("Blood Pressure Stage: " + BPstage)
+    // console.log("Number of Family Diseases: " + numDiseases)
 }
 
 function rdMetFn() {
