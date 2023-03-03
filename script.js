757308aa6299db19519c9f7460a6bc5ca11b6b60
Logan <loganmedwards@lewisu.edu>
2023-03-02 20:16:36 -0600

    Added calcultaions from ft/in to m and lbs to kg

--- script.js
+++ script.js
@@ -6,24 +6,48 @@ async function wakeup() {
 
 function grabNumbers() {
     // Grab numbers from the text fields and pass them to the server
-    // All calculations are done outside of script.js
-    age = document.getElementById("Age").value;
-    feet = document.getElementById("HeightFt").value;
-    inches = document.getElementById("HeightIn").value;
-    weight = document.getElementById("Weight").value;
-    bpstage = document.getElementById("bplst").value;
+    // All point calculations are done outside of script.js
+    // age, height(meters), weight(kg), bloodPressure, diseases
+    let age = document.getElementById("Age").value;
+    let m;
+    let kg;
+    let bpstage = document.getElementById("bplst").value;
+
+    if (document.getElementById("metOne").checked == true) {
+        let cm = document.getElementById("HeightFt").value;
+        //cm -> m
+        m = cm / 100;
+    }
+    else{
+        let feet = document.getElementById("HeightFt").value;
+        let inches = document.getElementById("HeightIn").value;
+        //change feet -> inches -> cm -> meter
+        let totalInches = (feet * 12) + parseInt(inches);
+        let cm = (totalInches * 2.54);
+        m = cm / 100;
+    }
+
+    if (document.getElementById("metTwo").checked == true){
+        kg = document.getElementById("Weight").value;
+    }
+    else{
+        let lbs = document.getElementById("Weight").value;
+        kg = lbs /2.205;
+        kg = Math.round(kg * 10) / 10;
+    }
 
     // Each family disease is worth the same value of 10 points
     // This will return the number of disease as 0-3
-    numDiseases = 0;
+    let numDiseases = 0;
     if (document.getElementById("Diabetes").checked == true){ numDiseases++; }
     if (document.getElementById("Cancer").checked == true){ numDiseases++; }
     if (document.getElementById("Alzheimers").checked == true){ numDiseases++; }
 
     // Console log statements for testing feel free to uncomment 
-    // console.log("age: "+ age + " height: " + feet + "ft " + inches + "in");
-    // console.log("Weight : " + weight + "lbs");
-    // console.log("Blood Pressure Stage: " + BPstage)
+    // console.log("age: "+ age);
+    // console.log("Height: " + m + "m")
+    // console.log("Weight : " + kg + "kg");
+    // console.log("Blood Pressure Stage: " + bpstage)
     // console.log("Number of Family Diseases: " + numDiseases)
 }
 
